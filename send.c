d14d64a07eaea440403a9e2e67c9a24eb8e2a129
Cesar MH <56159783+cesar237@users.noreply.github.com>
2025-01-30 17:00:05 +0100

    minor fix

--- send.c
+++ send.c
@@ -312,7 +312,7 @@ void wg_packet_encrypt_worker(struct work_struct *work)
 static void wg_packet_create_data(struct wg_peer *peer, struct sk_buff *first)
 {
 	// struct wg_device *wg = peer->device;
-	int ret = -EINVAL;
+	int ret = 0;
 	struct sk_buff *skb, *next;
 	enum packet_state state = PACKET_STATE_CRYPTED;
 
