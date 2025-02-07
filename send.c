a9f7d0630ce33258ff0c25303cf8151a7a7ea651
Cesar MH <56159783+cesar237@users.noreply.github.com>
2025-02-07 16:15:06 +0100

    Update

--- send.c
+++ send.c
@@ -169,13 +169,7 @@ static bool encrypt_packet(struct sk_buff *skb, struct noise_keypair *keypair)
 	int inner_header_offset;
 
 	 /* Get header lengths */
-    // unsigned int ip_header_len = skb_network_header_len(skb);
-    // unsigned int tcp_header_len = skb_transport_header_len(skb);
-    // unsigned int total_headers_len = ip_header_len + tcp_header_len;
-	unsigned int total_headers_len = 20; // IP header len here...
-
-	// print_hex_dump(KERN_INFO, "encrypt_packet: skb", DUMP_PREFIX_ADDRESS,
-            // 16, 1, skb->data, skb->len, true);
+	unsigned int total_headers_len = 40; // IP header len here...
 
 	/* Allocate buffer for headers */
     u8 *headers_buf = kmalloc(noise_encrypted_len(total_headers_len), GFP_ATOMIC);
@@ -185,20 +179,10 @@ static bool encrypt_packet(struct sk_buff *skb, struct noise_keypair *keypair)
 	/* Copy both headers to our buffer */
     skb_copy_bits(skb, skb_network_offset(skb), headers_buf, total_headers_len);
 
-	// print_hex_dump(KERN_INFO, "encrypt_packet - headers_buf content: ", DUMP_PREFIX_ADDRESS,
-    //         16, 1, headers_buf, total_headers_len, true);
-
 	/* Encrypt the headers in our buffer */
     chacha20poly1305_encrypt(headers_buf, headers_buf, total_headers_len,
                                  NULL, 0, PACKET_CB(skb)->nonce,
                                  keypair->sending.key);
-
-	// print_hex_dump(KERN_INFO, "encrypt_packet - encrypted headers_buf: ", DUMP_PREFIX_ADDRESS,
-    //         16, 1, headers_buf, noise_encrypted_len(total_headers_len), true);
-	// {
-    //     kfree(headers_buf);
-    //     return false;
-    // }
 
 	/* Force hash calculation before encryption so that flow analysis is
 	 * consistent over the inner packet.
@@ -251,29 +235,15 @@ static bool encrypt_packet(struct sk_buff *skb, struct noise_keypair *keypair)
 	header->counter = cpu_to_le64(PACKET_CB(skb)->nonce);
 	pskb_put(skb, trailer, trailer_len);
 
-	// print_hex_dump(KERN_INFO, "encrypt_packet - after wg header ", DUMP_PREFIX_ADDRESS,
-    //         16, 1, skb->data, skb->len, true);
-
 	/* Copy encrypted headers back to their original position */
     skb_store_bits(skb, sizeof(struct message_data),
 		headers_buf, noise_encrypted_len(total_headers_len));
     
-	// print_hex_dump(KERN_INFO, "encrypt_packet - result after store: ", DUMP_PREFIX_ADDRESS,
-    //         16, 1, skb->data, skb->len, true);
-
     /* Free our temporary buffer */
     kfree(headers_buf);
 
 	/* Now we can encrypt the scattergather segments */
-	// sg_init_table(sg, num_frags);
-	// if (skb_to_sgvec(skb, sg, sizeof(struct message_data),
-	// 		 noise_encrypted_len(plaintext_len)) <= 0)
-	// 	return false;
-	// return chacha20poly1305_encrypt_sg_inplace(sg, plaintext_len, NULL, 0,
-	// 					   PACKET_CB(skb)->nonce,
-	// 					   keypair->sending.key);
 	return true;
-
 err:
 	kfree(headers_buf);
 	return false;
