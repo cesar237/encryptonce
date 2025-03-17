8605b76a6b10495ab7248c8f845f8f8f4f6185b2
Cesar MH <56159783+cesar237@users.noreply.github.com>
2025-03-17 16:42:44 +0100

    fix

--- send.c
+++ send.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
  */
 
+#define DEFINE_WG_PARAMS
 #include "queueing.h"
 #include "timers.h"
 #include "device.h"
