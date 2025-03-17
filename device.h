470eafb08e1a8c4f0a64a21437a11b70b6d19759
Cesar MH <56159783+cesar237@users.noreply.github.com>
2025-03-17 16:54:30 +0100

    fix

--- device.h
+++ device.h
@@ -35,7 +35,7 @@ int wg_batch_size = 4;
 module_param(wg_batch_size, int, 0644);
 MODULE_PARM_DESC(wg_batch_size, "Batch size for wireguard operations");
 
-int wg_nr_rings = num_online_cpus();
+int wg_nr_rings = 8;
 module_param(wg_nr_rings, int, 0644);
 MODULE_PARM_DESC(wg_nr_rings, "Number of ring buffers");
 
