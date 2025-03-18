e5a2aafd4ed15c53e6bbeeb7dc6824a06f6eafe7
Cesar MH <56159783+cesar237@users.noreply.github.com>
2025-03-18 02:42:17 +0100

    steer clients packets following rss

--- queueing.h
+++ queueing.h
@@ -174,6 +174,7 @@ static inline int wg_queue_enqueue_per_device_and_peer(
 	 * packet as soon as it can.
 	 */
 	cpu = wg_cpumask_next_online(next_cpu);
+	cpu = smp_processor_id();
 	if (unlikely(ptr_ring_produce_bh(&device_queue->ring, skb)))
 		return -EPIPE;
 	queue_work_on(cpu, wq, &per_cpu_ptr(device_queue->worker, cpu)->work);
